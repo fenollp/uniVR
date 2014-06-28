41a3ab708bbd40af905a904a08655134eb4ad7dd
Pierre Fenoll <pierrefenoll@gmail.com>
2014-06-28 22:39:21 +0200

    algo_detection_based_tracker/

--- algorithms/algo_basic_haar/main.cc
+++ algorithms/algo_basic_haar/main.cc
@@ -17,68 +17,62 @@ main (int argc, const char* argv[])
 {
     CvCapture* capture = NULL;
     cv::Mat frameIn, frameCopy;
+    cv::CascadeClassifier cascade;
 
-    cv::CascadeClassifier cascade, nestedCascade;
-
-    if (!nestedCascade.load(NESTED_CASCADE_NAME))
-        std::cerr << "!load " NESTED_CASCADE_NAME << std::endl;
     if (!cascade.load(CASCADE_NAME)) {
         std::cerr << "!load " CASCADE_NAME << std::endl;
         return 1;
     }
 
-    if (!(capture = cvCaptureFromCAM(0)))
+    if (!(capture = cvCaptureFromCAM(0))) {
         std::cerr << "!cap from webcam 0" << std::endl;
+        return 2;
+    }
 //capture = cvCaptureFromAVI("file.avi");
 
     cvNamedWindow(ALGO, 1);
 
-    if (capture) {
-        std::cout << "In capture ..." << std::endl;
+    while (true) {
+        IplImage* iplImg = cvQueryFrame(capture);
+        frameIn = cv::cvarrToMat(iplImg);
+        if (frameIn.empty())
+            break;
+        if (iplImg->origin == IPL_ORIGIN_TL)
+            frameIn.copyTo(frameCopy);
+        else
+            cv::flip(frameIn, frameCopy, 0);
 
-        while (true) {
-            IplImage* iplImg = cvQueryFrame(capture);
-            frameIn = cv::cvarrToMat(iplImg);
-            if (frameIn.empty())
-                break;
-            if (iplImg->origin == IPL_ORIGIN_TL)
-                frameIn.copyTo(frameCopy);
-            else
-                cv::flip(frameIn, frameCopy, 0);
+        std::vector<cv::Rect> faces;
+        cv::Mat gray, thumb(cvRound(frameCopy.rows / SCALE),
+                            cvRound(frameCopy.cols / SCALE), CV_8UC1);
 
-            std::vector<cv::Rect> faces;
-            cv::Mat gray, thumb(cvRound(frameCopy.rows / SCALE),
-                                cvRound(frameCopy.cols / SCALE), CV_8UC1);
+        cv::cvtColor(frameCopy, gray, CV_BGR2GRAY);
+        cv::resize(gray, thumb, thumb.size(), 0, 0, cv::INTER_LINEAR);
+        cv::equalizeHist(thumb, thumb);
 
-            cv::cvtColor(frameCopy, gray, CV_BGR2GRAY);
-            cv::resize(gray, thumb, thumb.size(), 0, 0, cv::INTER_LINEAR);
-            cv::equalizeHist(thumb, thumb);
+        auto s = std::chrono::high_resolution_clock::now();
+        cascade.detectMultiScale(thumb, faces,
+                                 1.1, 2, 0
+                                 //| cv::CASCADE_FIND_BIGGEST_OBJECT
+                                 //|cv::CASCADE_DO_ROUGH_SEARCH
+                                 | cv::CASCADE_SCALE_IMAGE,
+                                 cv::Size(30, 30));
+        auto f = std::chrono::high_resolution_clock::now();
+        auto lapse = std::chrono::duration_cast<std::chrono::nanoseconds>(f-s);
+        std::cout << "\t" << lapse.count() << "ns" << std::endl;
 
-            auto s = std::chrono::high_resolution_clock::now();
-            cascade.detectMultiScale(thumb, faces,
-                                     1.1, 2, 0
-                                     //| cv::CASCADE_FIND_BIGGEST_OBJECT
-                                     //|cv::CASCADE_DO_ROUGH_SEARCH
-                                     | cv::CASCADE_SCALE_IMAGE,
-                                     cv::Size(30, 30));
-            auto f = std::chrono::high_resolution_clock::now();
-            auto lapse = std::chrono::duration_cast<std::chrono::nanoseconds>(f-s);
-            std::cout << "\t" << lapse.count() << "ns" << std::endl;
-
-            for (const cv::Rect& f : faces) {
-                cv::Point center(cvRound((f.x + f.width * 0.5) * SCALE),
-                                 cvRound((f.y + f.height * 0.5) * SCALE));
-                int r = cvRound((f.width + f.height) * 0.5 * 0.5 * SCALE);
-                cv::circle(frameCopy, center, r, CV_RGB(0,128,255), 3, 8, 0);
-            }
-            cv::imshow(ALGO, frameCopy);
-
-            if (cv::waitKey(10) >= 0)
-                cvReleaseCapture(&capture);
+        for (const cv::Rect& f : faces) {
+            cv::Point center(cvRound((f.x + f.width * 0.5) * SCALE),
+                             cvRound((f.y + f.height * 0.5) * SCALE));
+            int r = cvRound((f.width + f.height) * 0.5 * 0.5 * SCALE);
+            cv::circle(frameCopy, center, r, CV_RGB(0,128,255), 3, 8, 0);
         }
-        cv::waitKey(0);
-        cvReleaseCapture(&capture);
+        cv::imshow(ALGO, frameCopy);
+
+        if (cv::waitKey(10) >= 0)
+            break;
     }
+    cvReleaseCapture(&capture);
     cvDestroyWindow(ALGO);
 
     return 0;
---
+++ algorithms/algo_detection_based_tracker/main.cc
@@ -0,0 +1,52 @@
+#define ALGO "algo_detection_based_tracker"
+
+#include <opencv2/contrib/detection_based_tracker.hpp> // = EXPERIMENTAL
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+#include <iostream>
+
+// Config
+#define CASCADE_NAME "xml/haarcascade_frontalface_alt.xml"
+#define SCALE 1.1
+
+int
+main (int argc, const char* argv[])
+{
+    std::vector<cv::Rect_<int> > faces; // Note "Rect_"
+    DetectionBasedTracker::Parameters param;
+    param.maxObjectSize = 400;
+    param.maxTrackLifetime = 20;
+    param.minDetectionPeriod = 7;
+    param.minNeighbors = 3;
+    param.minObjectSize = 20;
+    param.scaleFactor = SCALE;
+
+    auto obj = cv::DetectionBasedTracker(CASCADE_NAME, param);
+    obj.run();
+    cv::VideoCapture cap(0);
+    cv::Mat img, gray;
+    cv::namedWindow(ALGO, 1);
+
+    while (true) {
+        cap >> img;
+        cv::cvtColor(img, gray, CV_RGB2GRAY);
+
+        auto s = std::chrono::high_resolution_clock::now();
+        obj.process(gray);
+        obj.getObjects(faces);
+        auto f = std::chrono::high_resolution_clock::now();
+        auto lapse = std::chrono::duration_cast<std::chrono::nanoseconds>(f-s);
+        std::cout << "\t" << lapse.count() << "ns" << std::endl;
+
+        // if(faces.size() == 0) obj.resetTracking();
+        for (const auto& face : faces)
+            cv::rectangle(img, face, CV_RGB(0,255,0), 3);
+        cv::imshow(ALGO, img);
+        if (cv::waitKey(10) >= 0)
+            break;
+    }
+    obj.stop();
+
+    return 0;
+}
