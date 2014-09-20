49f5d955965a9a475bbcf1555d5adf12449a5d98
Pierre Fenoll <pierrefenoll@gmail.com>
2014-09-20 02:31:44 +0200

    remove old x11 jabber. simplify nvr stuff

--- nvr/src/main.cc
+++ nvr/src/main.cc
@@ -11,6 +11,9 @@
 
 #include <iostream>
 
+#define WINDOW "nvr"
+
+
 int
 main (int argc, char** argv) {
     try {
@@ -19,8 +22,8 @@ main (int argc, char** argv) {
         // Dlib comes with example images in the examples/faces folder so give
         // those as arguments to this program.
         if (argc == 1) {
-            std::cout << "Call this program like this:" << std::endl;
-            std::cout << "./nvr 68_face_landmarks.dat face.jpg" << std::endl;
+            std::cout << "Call this program like this:" << std::endl
+                      << "./nvr 68_face_landmarks.dat" << std::endl;
             return 1;
         }
 
@@ -30,7 +33,7 @@ main (int argc, char** argv) {
             dlib::get_frontal_face_detector();
         // We also need a shape_predictor. It will predict face landmark
         // positions given an image and face bounding box.  Here we are loading
-        // the model from the 68_face_landmarks.dat file given via CLI.
+        // the model from the 68_face_landmarks.dat file given.
         dlib::shape_predictor sp;
         dlib::deserialize(argv[1]) >> sp;
 
@@ -40,27 +43,23 @@ main (int argc, char** argv) {
             return 2;
         }
 
-        cv::namedWindow("nvr");
-        cv::Mat frame;
+        cv::namedWindow(WINDOW, 1);
 
         while (true) {
+            cv::Mat frame;
             capture >> frame;
             if (frame.empty())
                 break;
 
-            // dlib::array2d<dlib::rgb_pixel>
-            //     img( dlib::mat<dlib::rgb_pixel>(
-            //              dlib::cv_image<dlib::rgb_pixel>(frame)) );
-            //dlib::array2d<dlib::rgb_pixel> img(frame.rows, frame.cols);
-            // dlib::cv_image<dlib::bgr_pixel> cvimg(frame);
-            // dlib::array2d<dlib::bgr_pixel> img(cvimg);
-            typedef dlib::array2d<dlib::rgb_pixel>  rgb_2d;
-            typedef dlib::cv_image<dlib::bgr_pixel> rgb_cv;
-            rgb_2d img;
-            rgb_cv cvimg(frame);
-            dlib::assign_image<rgb_2d,rgb_cv>(img, frame);
+            // Can also pass cv_image objects into the detector directly & that
+            // will work fine as well. More generally, you can pass a cv_image
+            // object to any dlib image processing function that doesn't try to
+            // resize the image.
+            // Note: cv::Mat -> BGR, dlib's -> RGB.
+            dlib::array2d<dlib::rgb_pixel> img;
+            dlib::assign_image(img, dlib::cv_image<dlib::bgr_pixel>(frame));
             // Make the image larger so we can detect small faces.
-            dlib::pyramid_up(img);
+            ///dlib::pyramid_up(img);
 
             // Now tell the face detector to give us a list of bounding boxes
             // around all the faces in the image.
@@ -69,38 +68,24 @@ main (int argc, char** argv) {
 
             // Now we will go ask the shape_predictor to tell us the pose of
             // each face we detected.
-            std::vector<dlib::full_object_detection> shapes;
             for (size_t j = 0; j < dets.size(); ++j) {
-                dlib::full_object_detection shape = sp(img, dets[j]);
-                // std::cout << "# Parts: "<< shape.num_parts()
-                //           << std::endl << "pixel position of first part:  "
-                //           << shape.part(0)
-                //           << std::endl << "pixel position of second part: "
-                //           << shape.part(1) << std::endl;
-                // You get the idea, you can get all the face part locations if
-                // you want them.  Here we just store them in shapes so we can
-                // put them on the screen.
-                shapes.push_back(shape);
+                dlib::full_object_detection face = sp(img, dets[j]);
+                size_t num_parts = face.num_parts();
+                for (size_t k = 0; k < num_parts; ++k) {
+                    const dlib::point p = face.part(k);
+                    if (p == dlib::OBJECT_PART_NOT_PRESENT)
+                        continue;
+                    cv::Point pcv(p.x(), p.y());
+                    cv::line(frame, pcv,pcv, cv::Scalar(255,255,255), 4, 8,0);
+                }
             }
 
-            // Now lets view our face poses on the screen.
-            std::vector<dlib::image_window::overlay_line> rendered_dets =
-                dlib::render_face_detections(shapes);
-            std::cout << "Hit enter to process the next image..." << std::endl;
-            for (auto& pair : rendered_dets) {
-                cv::Point p1 = cv::Point(pair.p1.x(), pair.p1.y());
-                cv::Point p2 = cv::Point(pair.p2.x(), pair.p2.y());
-                cv::line(frame, p1, p1, cv::Scalar(255,255,255), 4, 8, 0);
-                cv::line(frame, p2, p2, cv::Scalar(255,255,255), 8, 8, 0);
-            }
-            cv::imshow("nvr", frame);
-            //while (cv::waitKey() != 'q') ;
-            std::cin.get();
+            cv::imshow(WINDOW, frame);
+            //std::cin.get();
         }
     }
-    catch (std::exception& e)
-    {
-        std::cout << "\nexception thrown!" << std::endl;
-        std::cout << e.what() << std::endl;
+    catch (std::exception& e) {
+        std::cout << std::endl << "exception thrown!" << std::endl
+                  << e.what() << std::endl;
     }
 }
