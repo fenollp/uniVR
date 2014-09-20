b75b81c16aed40801c4cbe98f1ce2ec1470fc426
Pierre Fenoll <pierrefenoll@gmail.com>
2014-09-20 16:09:24 +0200

    nvr drop some frames & use a ROI centered on previously-found nose.

--- README.md
+++ README.md
@@ -12,6 +12,17 @@ Then it blows your mind, thank you.
 Johnny Chung Lee did this with his “[Head Tracking for Desktop VR Displays using the WiiRemote](http://www.youtube.com/watch?v=Jd3-eiid-Uw&t=2m30s)” project.
 He also did a [TED talk](http://youtu.be/0H1zrLZwPjQ?t=3m41s) about it.
 
+#### Comparison of some algorithms
+* [Comparison of feature descriptors](http://computer-vision-talks.com/articles/2011-01-28-comparison-of-feature-descriptors/)
+    * => LAZY > SURF >> BRIEF
+* [SO: feature detectors and descriptors comparison](http://stackoverflow.com/questions/18437878/feature-detectors-and-descriptors-comparison)
+    * SIFT, SURF only similarity-invariant (homotecies)
+    * affine covariance is important too (invariance to viewpoint changes)
+    * Hessian-Affine, MSER are affine-invariant but as slow as SIFT, SURF
+    * FAST > SURF > SIFT
+    * BRIEF, FREAK faster but worse
+    * for realtime: use GPU | BRIEF, FREAK
+
 ## Requirements
 This code should compile | run with
 
@@ -22,11 +33,21 @@ This code should compile | run with
 
 ### nvr/*
 
-`cd nvr && make`
+`cd nvr && make && ./nvr shape_predictor_68_face_landmarks.dat`
 
-* Uses [Face Alignment at 3000 FPS via Regressing Local Binary Features](http://research.microsoft.com/en-US/people/yichenw/cvpr14_facealignment.pdf)
-    * Really its public implementation [One Millisecond Face Alignment with an Ensemble of Regression Trees](http://www.csc.kth.se/~vahidk/papers/KazemiCVPR14.pdf)
-* Related database (of still images): [helen](http://www.ifp.illinois.edu/~vuongle2/helen/)
+* **facial landmark detection** using [C++ Dlib](http://dlib.net/)
+* **Face detector:** Histogram of Oriented Gradients
+    * Affine-invariant feature descriptor (like SIFT, SURF)
+        * **However good enough in practice**
+    * First paper by INRIA in 2005
+        * *Histograms of Oriented Gradients for Human Detection by Navneet Dalal and Bill Triggs, CVPR 2005*
+    * "Augmented the method slightly to use the version of HOG features from:"
+        * *Object Detection with Discriminatively Trained Part Based Models by P. Felzenszwalb, R. Girshick, D. McAllester, D. Ramanan IEEE Transactions on Pattern Analysis and Machine Intelligence, Vol. 32, No. 9, Sep. 2010*
+* **Feature extractor | Shape predictor:**
+    * [Face Alignment at 3000 FPS via Regressing Local Binary Features](http://research.microsoft.com/en-US/people/yichenw/cvpr14_facealignment.pdf)
+        * Really its public implementation: [One Millisecond Face Alignment with an Ensemble of Regression Trees](http://www.csc.kth.se/~vahidk/papers/KazemiCVPR14.pdf)
+        * Both papers dating 2014
+    * Related database (of still images): [helen](http://www.ifp.illinois.edu/~vuongle2/helen/)
 
 
 ### algorithms/*
@@ -34,6 +55,7 @@ This code should compile | run with
 `cd algorithms && make _/ && ./compare.sh`
 
 Those are tests comparing speed and accuracy of OpenCV's implementation of the state-of-the-art.
+
 
 ### stateoftheart/*
 
--- nvr/src/main.cc
+++ nvr/src/main.cc
@@ -12,10 +12,11 @@
 #include <iostream>
 
 #define WINDOW "nvr"
+#define DROP_AMOUNT 15
 
 
 int
-main (int argc, char** argv) {
+main (int argc, const char* argv[]) {
     try {
         // Takes in a shape model file and then a list of images to
         // process.  We will take these filenames in as command line arguments.
@@ -29,8 +30,7 @@ main (int argc, char** argv) {
 
         // We need a face detector.  We will use this to get bounding boxes for
         // each face in an image.
-        dlib::frontal_face_detector detector =
-            dlib::get_frontal_face_detector();
+        auto detector = dlib::get_frontal_face_detector();
         // We also need a shape_predictor. It will predict face landmark
         // positions given an image and face bounding box.  Here we are loading
         // the model from the 68_face_landmarks.dat file given.
@@ -43,10 +43,13 @@ main (int argc, char** argv) {
             return 2;
         }
 
+        cv::Mat frame;
         cv::namedWindow(WINDOW, 1);
 
+        std::vector<dlib::rectangle> dets;
+        std::vector<dlib::full_object_detection> shapes;
+        size_t i = 0;
         while (true) {
-            cv::Mat frame;
             capture >> frame;
             if (frame.empty())
                 break;
@@ -56,36 +59,56 @@ main (int argc, char** argv) {
             // object to any dlib image processing function that doesn't try to
             // resize the image.
             // Note: cv::Mat -> BGR, dlib's -> RGB.
+            dlib::cv_image<dlib::bgr_pixel> imgcv(frame);
             dlib::array2d<dlib::rgb_pixel> img;
-            dlib::assign_image(img, dlib::cv_image<dlib::bgr_pixel>(frame));
+            dlib::assign_image(img, imgcv);
             // Make the image larger so we can detect small faces.
             ///dlib::pyramid_up(img);
 
-            // Now tell the face detector to give us a list of bounding boxes
-            // around all the faces in the image.
-            std::vector<dlib::rectangle> dets = detector(img);
-            std::cout << "# Faces detected: " << dets.size() << std::endl;
+            dets.clear();
+            if (i % DROP_AMOUNT == 0) {
+                // Run the full detector every now and then
+                // Tell the face detector to give us a list of bounding boxes
+                // around all the faces in the image.
+                dets = detector(img);
+                std::cout << "# Faces detected: " << dets.size() << std::endl;
+            } else {
+                // For most frames just guess where the face is
+                // based on the last one
+                for (const auto& face : shapes) {
+                    // Get a square box centered on the nose
+                    dlib::rectangle rect;
+                    for (size_t j = 0; j < face.num_parts(); ++j)
+                        rect += face.part(j);
+                    const auto& nose = face.part(30);
+                    dets.push_back( // ROI
+                        dlib::centered_rect(nose, rect.width(), rect.width()));
+                }
+                std::cout << "Dropped frame" << std::endl;
+            }
 
+            shapes.clear();
             // Now we will go ask the shape_predictor to tell us the pose of
             // each face we detected.
-            for (size_t j = 0; j < dets.size(); ++j) {
-                dlib::full_object_detection face = sp(img, dets[j]);
-                size_t num_parts = face.num_parts();
-                for (size_t k = 0; k < num_parts; ++k) {
-                    const dlib::point p = face.part(k);
+            for (const auto& det : dets) {
+                const auto& face = sp(img, det);
+                for (size_t k = 0; k < face.num_parts(); ++k) {
+                    const auto& p = face.part(k);
                     if (p == dlib::OBJECT_PART_NOT_PRESENT)
                         continue;
                     cv::Point pcv(p.x(), p.y());
                     cv::line(frame, pcv,pcv, cv::Scalar(255,255,255), 4, 8,0);
                 }
+                shapes.push_back(face);
             }
 
             cv::imshow(WINDOW, frame);
             //std::cin.get();
+            ++i;
         }
     }
     catch (std::exception& e) {
-        std::cout << std::endl << "exception thrown!" << std::endl
-                  << e.what() << std::endl;
+        std::cout << std::endl << "exception thrown!"
+                  << std::endl << e.what() << std::endl;
     }
 }
