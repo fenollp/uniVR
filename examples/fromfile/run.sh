106450694c3d3ed390c64cc102beed5c4dcd89c4
Pierre Fenoll <pierrefenoll@gmail.com>
2016-09-03 17:10:38 +0200

    json: finish up bench script

--- examples/fromfile/run.sh
+++ examples/fromfile/run.sh
@@ -1,10 +1,26 @@
-#!/bin/bash
+#!/bin/bash -e
 
-# Usage: $0  <video.mp4>
+[[ $# -ne 1 ]] && echo "Usage: $0  <video.mp4>" && exit 1
 
-FROMFILE="${FROMFILE:-./build/bin/fromfile}"
+function run {
+    local FROMFILE="${FROMFILE:-./build/bin/fromfile}"
+    local JSONS="${JSONS:-../testdata-univr.git/jsons}"
 
-$FROMFILE \
-    "$(git describe --abbrev --dirty --always --tags)" \
-    "$(hostname -f)" \
-    "$1"
+    local gv="$(git describe --abbrev --dirty --always --tags)"
+    local vid="$1"
+    local fqdn="$(hostname -f)"
+
+    local tmp=$RANDOM.json
+
+    $FROMFILE \
+        "$gv" \
+        "$fqdn" \
+        "$vid" \
+        2> $tmp
+
+    local path="$JSONS/$gv/$(basename "$vid")"
+    mkdir -p "$path"
+    mv $tmp "$path/$fqdn.json"
+}
+
+run "$@"
