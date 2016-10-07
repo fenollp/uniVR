fad4a1d9a2a4f9d678c567ff3a772e1ba0f67053
Pierre Fenoll <pierrefenoll@gmail.com>
2016-10-07 13:51:59 +0200

    sentiment: use asserts. dump to json instead of csv

--- examples/sentiment/ckplus.py
+++ examples/sentiment/ckplus.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python2
 
 from __future__ import print_function
+from skimage import io
 
 import dlib
 import sys
@@ -8,9 +9,8 @@ import os
 import glob
 import cv2
 import math
+import json
 import numpy as np
-
-from skimage import io
 
 
 if len(sys.argv) != 2:
@@ -18,13 +18,14 @@ if len(sys.argv) != 2:
     exit()
 
 
-viz = True
+viz = False  # :: boolean
+
 predictor_path = '../../data/ldmrks68.dat'
 ckplus_root = sys.argv[1]
 ckplus_emotion = 'Emotion'
 ckplus_images = 'cohn-kanade-images'
 ckplus_landmarks = 'Landmarks'
-ckplus_csv = os.path.join(ckplus_root, 'ckplus.csv')
+ckplus_json = os.path.join(ckplus_root, 'ckplus.json')
 MARK_NOSE_TOP = 27
 MARK_NOSE_TIP = 33
 SMOOTHING = 0.000000001
@@ -45,28 +46,38 @@ def e_and_imgs_from_txt(txt):
     ## Take the last images of the sequence. MIGHT fix too much on face!
     imgs = [os.path.join(imgs_root, name(i)) for i in range(int(last) // 2, int(last) + 1)]
     with open(txt, 'r') as ftxt:
-        E = float(ftxt.read().strip())
+        E = int(float(ftxt.read().strip()))
         return E, imgs
 
+def E_to_emotion(E):
+    return {0: 'neutral',
+            1: 'anger',
+            2: 'contempt',
+            3: 'disgust',
+            4: 'fear',
+            5: 'happy',
+            6: 'sadness',
+            7: 'surprise'
+    }[E].upper()
 
 def landmarks_from_img(img):
     dir1, dir2, idxpng = img.split(os.sep)[-1].split('_')
     idx, _ = idxpng.split('.')
     name = dir1 + '_' + dir2 + '_' + idx + '_landmarks.txt'
     path = os.path.join(ckplus_root, ckplus_landmarks, dir1, dir2, name)
-    Ls = []
+    xLs, yLs = [], []
     with open(path, 'r') as ftxt:
         for line in ftxt:
             x, y = line.split()
-            Ls.append(float(x))
-            Ls.append(float(y))
-    return Ls
+            xLs.append(float(x))
+            yLs.append(float(y))
+    return xLs, yLs
 
 
 def beta(xTop, yTop, xTip, yTip):
     b = math.atan((yTop - yTip) / (xTop - xTip + SMOOTHING))
     b += math.pi/2 if b < 0 else -math.pi/2
-    print('beta', "{:.3f}".format(b * 180 / math.pi))
+    print('delta to nose:', "{:.3f}".format(b * 180 / math.pi))
     return b
 
 def normalize(Xs, Ys):
@@ -83,16 +94,15 @@ def normalize(Xs, Ys):
     return landmarks
 
 
-ckplus_csv = open(ckplus_csv, 'wa')
+JSON = []
 
-# for txt in glob.glob(os.path.join(ckplus_root, ckplus_emotion, 'S071', '005', '*.txt')):
 for txt in glob.glob(os.path.join(ckplus_root, ckplus_emotion, '*', '*', '*.txt')):
     E, imgs = e_and_imgs_from_txt(txt)
-    print('\t', "E imgs", E, len(imgs))
+    print('\t', 'E', E_to_emotion(E), 'imgs', len(imgs))
 
-    for j, f in enumerate(imgs):
-        print("Processing file:", f)
-        img = io.imread(f)
+    for fimg in imgs:
+        print('Processing', fimg)
+        img = io.imread(fimg)
 
         if viz:
             win.clear_overlay()
@@ -106,10 +116,9 @@ for txt in glob.glob(os.path.join(ckplus_root, ckplus_emotion, '*', '*', '*.txt'
         box = sorted(dets, key=lambda rect: rect.area(), reverse=True)[0]
         shape = predictor(img, box)
 
-        Ls = landmarks_from_img(f)
-        print('#landmarks', len(Ls))
-        if len(Ls) != 2*68:
-            exit()
+        if True:
+            xLs, yLs = landmarks_from_img(fimg)
+            assert len(xLs) == 68 and 68 == len(yLs)
 
         xTip = shape.part(MARK_NOSE_TIP).x
         yTip = shape.part(MARK_NOSE_TIP).y
@@ -124,14 +133,18 @@ for txt in glob.glob(os.path.join(ckplus_root, ckplus_emotion, '*', '*', '*.txt'
             Xs.append(float(part.x))
             Ys.append(float(part.y))
             if viz:
-                ptxt = (int(Ls[PLs]), int(Ls[PLs+1]))
-                pdet = (part.x, part.y)
+                # ptxt = (int(Ls[PLs]), int(Ls[PLs+1]))
                 # cv2.line(wLs, ptxt, ptxt, (0,255,255), 2)
+                pdet = (part.x, part.y)
                 cv2.line(wLs, pdet, pdet, (255,255,0), 2)
             PLs += 2
+        assert len(Xs) == 68 and 68 == len(Ys)
 
         NormLs = normalize(Xs, Ys)
-        print('#NormLs', len(NormLs))
+        assert len(NormLs) == 2 * 2 * 68
+        delta = beta(NormLs[2+4*MARK_NOSE_TOP], NormLs[3+4*MARK_NOSE_TOP],
+                     NormLs[2+4*MARK_NOSE_TIP], NormLs[3+4*MARK_NOSE_TIP])
+        assert 0 == abs(int(delta * 180 / math.pi))
         if viz:
             pTip = (int(Xs[MARK_NOSE_TIP]), int(Ys[MARK_NOSE_TIP]))
             pTop = (int(Xs[MARK_NOSE_TOP]), int(Ys[MARK_NOSE_TOP]))
@@ -148,12 +161,6 @@ for txt in glob.glob(os.path.join(ckplus_root, ckplus_emotion, '*', '*', '*.txt'
                 PLs += 4
             cv2.line(wLs, (pTip[0],0), (pTip[0],512), (255,255,255), 1)
             cv2.line(wLs, (0,pTip[1]), (512,pTip[1]), (255,255,255), 1)
-            delta = beta(NormLs[2+4*MARK_NOSE_TOP], NormLs[3+4*MARK_NOSE_TOP],
-                         NormLs[2+4*MARK_NOSE_TIP], NormLs[3+4*MARK_NOSE_TIP])
-            delta_offset = abs(int(delta * 180 / math.pi))
-            print('new beta offset', delta_offset)
-            if delta_offset != 0:
-                exit()
 
         if viz:
             win.add_overlay(shape)
@@ -162,14 +169,23 @@ for txt in glob.glob(os.path.join(ckplus_root, ckplus_emotion, '*', '*', '*.txt'
 
         if viz:
             cv2.imshow("wLs", wLs)
-            # cv2.waitKey(0)
-        # exit()
+            cv2.waitKey(0)
 
-        line = str(E)
-        for part in shape.parts():
-            line += ',' + str(part.x) + ',' + str(part.y)
-        print(line, file=ckplus_csv)
+        Ls = []
+        PLs = 0
+        for part in xrange(0, len(NormLs) // 4):
+            # MIGHT loose interesting precision! (float -> int)
+            Ls.append({'x': int(NormLs[PLs+2]), 'y': int(NormLs[PLs+3])})
+            PLs += 4
+        assert 68 == len(Ls)
+        fn = fimg.split(os.sep)[-1]
+        assert 0 != len(fn)
+        JSON.append({'fn': fn,
+                     'ls': Ls,
+                     'e': E})
 
-ckplus_csv.close()
+
+with open(ckplus_json, 'w') as jout:
+    json.dump(JSON, jout)
 
 print('done')
