932c828082d16c7c0b76242cc02fd2576f802c36
Pierre Fenoll <pierrefenoll@gmail.com>
2016-10-11 12:53:42 +0200

    sentiment: add debug code for training

--- examples/sentiment/train_svm.py
+++ examples/sentiment/train_svm.py
@@ -16,16 +16,19 @@ if len(sys.argv) != 2:
     print(sys.argv[0], "  <landmarks data.json>")
     exit(1)
 
+viz = True
+train_v_test = 0.8
 # seed random here
 
 def get_items(E):
     items = []
     with open(sys.argv[1], 'r') as fjson:
         for img, data in json.load(fjson).iteritems():
-            items.append((img, data))
+            if E == data['e']:
+                items.append((img, data))
     random.shuffle(items)
     count = len(items)
-    train_v_test = 0.4
+    print(u.E_to_emotion(E), 'count', count)
     train = items[:int(count * train_v_test)]
     test  = items[-int(count * (1 - train_v_test)):]
     return train, test
@@ -56,3 +59,45 @@ for i in xrange(1, 10+1):
     print('accuracy:', score)
     scores.append(score)
 print('mean accuracy:', np.mean(scores))
+
+
+if viz:
+    import cv2
+    facer, landmarker = u.detectors()
+    cam = cv2.VideoCapture(0)
+    if not cam.isOpened():
+        print('!cam')
+        exit()
+    while True:
+        err, img = cam.read()
+        if not err:
+            print('!err')
+            exit()
+
+        for box in facer(img, 1):
+            cv2.rectangle(img, (int(box.left()),int(box.top())), (int(box.right()),int(box.bottom())), (255,255,255), 1)
+            shape = landmarker(img, box)
+            Xs, Ys = [], []
+            for part in shape.parts():
+                p = (part.x, part.y)
+                Xs.append(float(p[0]))
+                Ys.append(float(p[1]))
+                cv2.line(img, p, p, (0,0,255), 2)
+            pMean, Normd = u.normalize(Xs, Ys)
+            i, Ls = 0, []
+            for part in xrange(0, len(Normd) // 4):
+                p = (int(Normd[i+2]), int(Normd[i+3]))
+                Ls.append({'m': int(Normd[i + 0]),
+                           'a': int(Normd[i + 1]),
+                           'x': int(Normd[i + 2]),
+                           'y': int(Normd[i + 3])})
+                cv2.line(img, p, p, (255,0,0), 2)
+                i += 4
+            [E] = clf.predict([u.polars(Ls)])
+            print('predicted', E)
+            loc = (int(box.left()), int(box.top()-20))
+            cv2.putText(img, u.E_to_emotion(E), loc , cv2.FONT_HERSHEY_SIMPLEX, .5, (255,255,255), 1)
+
+        cv2.imshow('sentiment', img)
+        if cv2.waitKey(1) == ord('q'):
+            break
